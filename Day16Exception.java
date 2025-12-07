9d22c1414cb069a5ec0f30248b1ac80ab798d5d2
Lamiya17 <gulievalamia86@gmail.com>
2025-12-07 16:21:29 +0400

    Create Day16Exception.java

---
+++ Day16Exception.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args)  {
+        Scanner sc= new Scanner(System.in);
+        String s = sc.nextLine();
+       try{ int n = Integer.parseInt(s);
+        System.out.println(n);}
+        catch(Exception e){
+            System.out.println("Bad String");
+        }
+    }
+}
